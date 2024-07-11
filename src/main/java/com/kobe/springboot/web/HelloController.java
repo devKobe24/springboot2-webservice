fdbd9ba010b403af11ca620a6141abdf0186015a
Kobe <dev.skyachieve91@gmail.com>
2024-07-11 16:39:39 +0900

    :sparkles: Feat: '@GetMapping으로 HelloResponseDto API 가져오기.'

--- src/main/java/com/kobe/springboot/web/HelloController.java
+++ src/main/java/com/kobe/springboot/web/HelloController.java
@@ -1,6 +1,8 @@
 package com.kobe.springboot.web;
 
+import com.kobe.springboot.web.dto.HelloResponseDto;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 // 컨트롤러를 JSON을 반환하는 컨트롤러로 만들어 줍니다.
@@ -13,5 +15,13 @@ public class HelloController {
 	@GetMapping("/hello")
 	public String hello() {
 		return "hello";
+	}
+
+	// @RequestParam
+		// 외부에서 API로 넘긴 파라미터를 가져오는 어노테이션입니다.
+		// 여기에서는 외부에서 name(@RequestParam("name")) 이란 이름으로 넘긴 파라미터를 메소드 파라미터 name(String name)에 저장하게 됩니다.
+	@GetMapping("/hello/dto")
+	public HelloResponseDto helloDto(@RequestParam("name") String name, @RequestParam("amount") int amount) {
+		return new HelloResponseDto(name, amount);
 	}
 }
