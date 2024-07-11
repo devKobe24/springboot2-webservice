145e999dd2242b33621e526e840b647fec6a8ec6
Kobe <dev.skyachieve91@gmail.com>
2024-07-12 06:14:29 +0900

    :sparkles: Feat: 'Posts 등록 기능 구현 및 테스트.'

---
+++ src/main/java/com/kobe/springboot/service/PostsService.java
@@ -0,0 +1,18 @@
+package com.kobe.springboot.service;
+
+import com.kobe.springboot.domain.posts.PostsRepository;
+import com.kobe.springboot.web.dto.PostsSaveRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class PostsService {
+	private final PostsRepository postsRepository;
+
+	@Transactional
+	public Long save(PostsSaveRequestDto requestDto) {
+		return postsRepository.save(requestDto.toEntity()).getId();
+	}
+}
