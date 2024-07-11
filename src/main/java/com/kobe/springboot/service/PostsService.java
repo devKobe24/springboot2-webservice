8c2836c5446034e5aea648594151f1df576e5631
Kobe <dev.skyachieve91@gmail.com>
2024-07-12 06:47:39 +0900

    :sparkles: Feat: 'Posts 수정,조회 기능 구현 및 테스트.'

--- src/main/java/com/kobe/springboot/service/PostsService.java
+++ src/main/java/com/kobe/springboot/service/PostsService.java
@@ -1,7 +1,10 @@
 package com.kobe.springboot.service;
 
+import com.kobe.springboot.domain.posts.Posts;
 import com.kobe.springboot.domain.posts.PostsRepository;
+import com.kobe.springboot.web.dto.PostsResponseDto;
 import com.kobe.springboot.web.dto.PostsSaveRequestDto;
+import com.kobe.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -14,5 +17,19 @@ public class PostsService {
 	@Transactional
 	public Long save(PostsSaveRequestDto requestDto) {
 		return postsRepository.save(requestDto.toEntity()).getId();
+	}
+
+	@Transactional
+	public Long update(Long id, PostsUpdateRequestDto requestDto) {
+		Posts posts = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시들이 없습니다. id=" + id));
+		posts.update(requestDto.getTitle(), requestDto.getContent());
+
+		return id;
+	}
+
+	public PostsResponseDto findById(Long id) {
+		Posts entity = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+
+		return new PostsResponseDto(entity);
 	}
 }
