d8435b6df4dcec942b5bddc9b45dcac7882e1ae5
Kobe <dev.skyachieve91@gmail.com>
2024-07-12 11:53:39 +0900

    :sparkles: Feat: '전체 조회 화면 구현 및 테스트.'

--- src/main/java/com/kobe/springboot/service/PostsService.java
+++ src/main/java/com/kobe/springboot/service/PostsService.java
@@ -2,9 +2,12 @@ package com.kobe.springboot.service;
 
 import com.kobe.springboot.domain.posts.Posts;
 import com.kobe.springboot.domain.posts.PostsRepository;
+import com.kobe.springboot.web.dto.PostsListResponseDto;
 import com.kobe.springboot.web.dto.PostsResponseDto;
 import com.kobe.springboot.web.dto.PostsSaveRequestDto;
 import com.kobe.springboot.web.dto.PostsUpdateRequestDto;
+import java.util.List;
+import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -31,5 +34,12 @@ public class PostsService {
 		Posts entity = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
 
 		return new PostsResponseDto(entity);
+	}
+
+	@Transactional(readOnly = true)
+	public List<PostsListResponseDto> findAllDesc() {
+		return postsRepository.findAllDesc().stream()
+				.map(PostsListResponseDto::new)
+				.collect(Collectors.toList());
 	}
 }
