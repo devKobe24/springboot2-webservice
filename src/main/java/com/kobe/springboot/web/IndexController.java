000475d6766a2a667ad3ad3680936abd6a6be855
Kobe <dev.skyachieve91@gmail.com>
2024-07-13 09:41:29 +0900

    :recycle: Refactor: '어노테이션 기반으로 코드 개선.'

--- src/main/java/com/kobe/springboot/web/IndexController.java
+++ src/main/java/com/kobe/springboot/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.kobe.springboot.web;
 
+import com.kobe.springboot.config.auth.LoginUser;
 import com.kobe.springboot.config.auth.dto.SessionUser;
 import com.kobe.springboot.service.PostsService;
 import com.kobe.springboot.web.dto.PostsResponseDto;
@@ -21,9 +22,11 @@ public class IndexController {
 	// Model
 		// 서버 템플릿 엔진에서 사용할 수 있는 객체를 저장할 수 있습니다.
 		// 여기서는 postsService.findAllDesc()로 가져온 결과를 posts로 index.mustache에 전달합니다.
-	public String index(Model model) {
+	// @LoginUser SessionUser user
+		// 기존에(User) httpSession.getAttribute("user")로 가져오던 세션 정보 값이 개선되었습니다.
+		// 이제는 어느 컨트롤러든지 @LoginUser만 사용하면 세션 정보를 가져올 수 있게 되었습니다.
+	public String index(Model model, @LoginUser SessionUser user) {
 		model.addAttribute("posts", postsService.findAllDesc());
-		SessionUser user = (SessionUser) httpSession.getAttribute("user");
 
 		if (user != null) {
 			model.addAttribute("userName", user.getName());
